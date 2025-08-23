bd59b0bab9e5b529191bf466b77f43a42da1a603
imBecky <hbinqian@gmail.com>
2025-08-23 11:47:07 +0800

    feat: attribute profile LiDAR

--- preprocess.py
+++ preprocess.py
@@ -7,57 +7,52 @@ from config import args
 import os
 import matplotlib
 import matplotlib.pyplot as plt
+from skimage.morphology import reconstruction, disk
+from skimage.filters import rank
 matplotlib.use('TkAgg')
 
+CUDA0 = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# ===================== 读取各模态数据 =======================
+# ===================== 读取各模态数据为tensor =======================
 HSI_file_path = os.path.join(args.data_dir, 'HSI.mat')
 LiDAR_file_path = os.path.join(args.data_dir, 'LiDAR.mat')
-RGB_file_path = os.path.join(args.data_dir, 'RGB.mat')
+RGB_file_path = './rgb_down.mat'
 with h5py.File(HSI_file_path, 'r') as file:
     key = list(file.keys())
     data = file[key[0]][()]
     data = data.T
-    HSI_dataset = torch.from_numpy(np.transpose(data, (2, 0, 1)))
+    HSI_tensor = torch.from_numpy(np.transpose(data, (2, 0, 1))).to(CUDA0)
 with h5py.File(LiDAR_file_path, 'r') as file:
     key = list(file.keys())
-    data = file[key[0]][()]
-    data = data.T
-    LiDAR_dataset = torch.from_numpy(data).unsqueeze(0)
+    LiDAR_np = file[key[0]][()]
+    LiDAR_np = LiDAR_np.T
+    LiDAR_tensor = torch.from_numpy(data).unsqueeze(0).to(CUDA0)
 
-RGB_np = sio.loadmat(RGB_file_path)['data']
+RGB_np = sio.loadmat(RGB_file_path)['rgb_down'].transpose(2, 0, 1)
+RGB_tensor = torch.from_numpy(RGB_np).to(CUDA0)
 
-# ===================== 处理RGB =======================
-H, W, C = RGB_np.shape
-print('原图尺寸：', H, W, C)
-# 转成 float32，范围 [0,1]
-RGB_dataset = torch.from_numpy(RGB_np).float() / 255.0
-RGB_dataset = RGB_dataset.unsqueeze(0)
-plt.figure(figsize=(6, 3))
-plt.title('Original RGB')
-plt.imshow(RGB_np.transpose(1, 2, 0))
-plt.axis('off')
-plt.tight_layout()
-plt.show()
+# ========= 对LiDAR做AP ============
+dsm = LiDAR_np.astype(np.float32)
+dsm = np.where(dsm == -1, np.nan, dsm)
+H, W = dsm.shape
 
-down = nn.Conv2d(3, 3, kernel_size=(3, 3), stride=(2, 2), padding=1, bias=False)
+# 阈值列表
+lambdas = np.arange(2, 22, 2)   # 2,4,...,20 → 10 个
 
-with torch.no_grad():
-    down.weight.zero_()
-    # 每个输出通道只取对应输入通道中心像素
-    down.weight[0, 0, 1, 1] = 1.0
-    down.weight[1, 1, 1, 1] = 1.0
-    down.weight[2, 2, 1, 1] = 1.0
-    down_tensor = down(RGB_dataset)   # (1, 3,H/2,W/2)
-down_np = down_tensor.squeeze(0).permute(1, 2, 0).numpy()
+profile = [dsm]                 # 第 0 层放原图
+se = disk(1)                    # 3×3 结构元
 
-plt.figure(figsize=(6, 3))
-plt.title('Downsampled RGB (Conv stride=2)')
-plt.imshow(down_np)
-plt.axis('off')
-plt.tight_layout()
-plt.show()
-# 5. 保存新的 mat -------------------------------------------------------------
-save_dict = {'rgb_down': (down_np*255).astype('uint8')}
-sio.savemat('rgb_down.mat', save_dict)
-print('已保存为 rgb_down.mat')
+for lam in lambdas:
+    # 开运算 (opening by reconstruction)
+    marker = dsm - lam
+    opened = reconstruction(marker, dsm, method='dilation')
+    profile.append(opened)
+
+    # 关运算 (closing by reconstruction)
+    marker = dsm + lam
+    closed = reconstruction(marker, dsm, method='erosion')
+    profile.append(closed)
+
+profile = np.stack(profile, axis=0)   # (21, H, W)
+sio.savemat('lidar_ap.mat', {'lidar_ap': profile})
+print()
